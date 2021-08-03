2af9d3da8465c01a8dd5e180ca6dde9e1bbb0c74
Andres <andres.maduro.xmarts@gmail.com>
2021-08-03 19:34:47 -0400

    replace hashtag command s

--- src/bot.js
+++ src/bot.js
@@ -1,34 +1,16 @@
-const CoinGecko = require('coingecko-api');
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
-const CoinGeckoClient = new CoinGecko();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const { isNumeric, costBreeding, symbols } = require('./helpers');
+
+const { getData, breeding, usdToCrypto, prices } = require('./service');
 
 
 bot.command('slp', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["smooth-love-potion"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} SLP = ${messageArray[1]*data["smooth-love-potion"].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of SLP: \n${data["smooth-love-potion"].usd} USD \n${data["smooth-love-potion"].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -36,24 +18,8 @@ bot.command('slp', async (ctx)=>{
 
 bot.command('axs', async (ctx)=>{
     try{
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["axie-infinity"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} AXS = ${messageArray[1]*data["axie-infinity"].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of AXS: \n${data["axie-infinity"].usd} USD \n${data["axie-infinity"].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -61,25 +27,8 @@ bot.command('axs', async (ctx)=>{
 
 bot.command('btc', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["bitcoin"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} BTC = ${messageArray[1]*data.bitcoin.usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of BTC: \n${data.bitcoin.usd} USD \n${data.bitcoin.eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -87,25 +36,8 @@ bot.command('btc', async (ctx)=>{
 
 bot.command('eth', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["ethereum"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} ETH = ${messageArray[1]*data.ethereum.usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of ETH: \n${data.ethereum.usd} USD \n${data.ethereum.eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -113,25 +45,8 @@ bot.command('eth', async (ctx)=>{
 
 bot.command('vet', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["vechain"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} BTC = ${messageArray[1]*data['vechain'].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of VET: \n${data['vechain'].usd} USD \n${data['vechain'].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -140,25 +55,8 @@ bot.command('vet', async (ctx)=>{
 
 bot.command('btt', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["bittorrent"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} BTT = ${messageArray[1]*data['bittorrent'].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of BTT: \n${data['bittorrent'].usd} USD \n${data['bittorrent'].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -166,25 +64,8 @@ bot.command('btt', async (ctx)=>{
 
 bot.command('ada', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["cardano"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} ADA = ${messageArray[1]*data['cardano'].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of ADA: \n${data['cardano'].usd} USD \n${data['cardano'].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -192,25 +73,8 @@ bot.command('ada', async (ctx)=>{
 
 bot.command('bnb', async (ctx)=>{
     try{
-        
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["binance-coin"],
-            vs_currencies: ['eur', 'usd']
-        });
-        const { data } = resp;
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 1){
-            if(isNumeric(messageArray[1])){
-                ctx.reply(`${messageArray[1]} BNB = ${messageArray[1]*data['binance-coin'].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply(
-                `Price of BNB: \n${data['binance-coin'].usd} USD \n${data['binance-coin'].eur} EUR`
-                );
-        }
+        const resp = await getData(ctx.update.message.text);
+        ctx.reply(resp);
     }catch(e){
         ctx.reply(`C kgo compa :,vv`)
     };
@@ -218,48 +82,18 @@ bot.command('bnb', async (ctx)=>{
 
 bot.command('prices',async (ctx)=> {
     try {
-        const resp = await CoinGeckoClient.simple.price({
-            ids: ["smooth-love-potion", "axie-infinity", "bitcoin", "ethereum"],
-            vs_currencies: ['eur', 'usd'],
-        });
-        const { data } = resp;
-        console.log(ctx.update.message.text);
-        ctx.reply(
-        `Price of SLP: \n${data["smooth-love-potion"].usd} USD \n${data["smooth-love-potion"].eur} EUR 
-        \nPrice of AXS: \n${data["axie-infinity"].usd} USD \n${data["axie-infinity"].eur} EUR
-        \nPrice of BTC: \n${data.bitcoin.usd} USD \n${data.bitcoin.eur} EUR
-        \nPrice of ETH: \n${data.ethereum.usd} USD \n${data.ethereum.eur} EUR`
-            );
-        
+        const message = await prices();
+        ctx.reply(message);
     } catch (error) {
-
         ctx.reply(`C kgo compa :,vv`);
-
     };
     
 })
 
 bot.command('usd', async (ctx)=> {
     try{
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 2){
-            const valOne = messageArray[1];
-            const symbol = messageArray[2].toLocaleLowerCase();
-            const currency = symbols[symbol];
-            if(isNumeric(valOne) && currency){
-                const resp = await CoinGeckoClient.simple.price({
-                    ids: currency,
-                    vs_currencies: ['usd'],
-                });
-                const { data } = resp;
-                ctx.reply(`${valOne} USD = ${valOne/data[currency].usd} ${symbol.toUpperCase()}`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply("Comando invalido");
-        }
+        const message = await usdToCrypto(ctx.update.message.text);
+        ctx.reply(message);
     }catch(e){
         ctx.reply("Comando invalido");
     }
@@ -268,28 +102,8 @@ bot.command('usd', async (ctx)=> {
 
 bot.command('breed', async (ctx)=> {
     try{
-        const message = ctx.update.message.text;
-        const messageArray = message.split(" ");
-        if (messageArray.length > 2){
-            const valOne = messageArray[1];
-            const valTwo = messageArray[2];
-            if(isNumeric(valOne) && valOne >= 0 && valOne < 7 
-                && isNumeric(valTwo) && valTwo >= 0 && valTwo < 7){
-                const resp = await CoinGeckoClient.simple.price({
-                    ids: ["smooth-love-potion", "axie-infinity"],
-                    vs_currencies: ['usd'],
-                });
-                const { data } = resp;
-                ctx.reply(`
-                ${(costBreeding[valOne] + costBreeding[valTwo])
-                    * data["smooth-love-potion"].usd
-                    + 4*data["axie-infinity"].usd} USD`);
-            }else{
-                ctx.reply("Comando invalido");
-            }
-        }else{
-            ctx.reply("Comando invalido");
-        }
+        const message = await breeding(ctx.update.message.text);
+        ctx.reply(message);
     }catch(e){
         ctx.reply("Comando invalido");
     }
---
+++ src/service.js
@@ -0,0 +1,109 @@
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+const { isNumeric, costBreeding, symbols } = require('./helpers');
+
+const callApi = async (coin, currency) => {
+    const resp = await CoinGeckoClient.simple.price({
+        ids: [coin],
+        vs_currencies: currency
+    });
+    return resp.data;
+}
+
+const getData = async (data) => {
+    const messageArray = data.split(" ");
+    const symbolCoin = messageArray[0].replace('/', '');
+    const coin = symbols[symbolCoin];
+    if(messageArray.length === 3){
+        const valCoin = parseInt(messageArray[1]);
+        const porcent = parseFloat(messageArray[2]);
+        if(isNumeric(valCoin) && isNumeric(porcent) && porcent > 0 && porcent <= 100){
+            const data =  await callApi(coin, ['usd'])
+            const value = valCoin*(porcent/100);
+            return `${value} ${symbolCoin.toUpperCase()}  = ${value*data[coin].usd} USD`;
+        }else{
+            return "Comando Invalido";
+        };
+    }else if(messageArray.length === 2){
+        const valCoin = parseInt(messageArray[1]);
+        if(isNumeric(valCoin)){
+            const data =  await callApi(coin, ['usd']);
+            return `${valCoin} ${symbolCoin.toUpperCase()} = ${valCoin*data[coin].usd} USD`;
+        }else{
+            return "Comando invalido";
+        }
+    }else{
+        const data =  await callApi(coin, ['usd', 'eur']);
+        return `Price of ${symbolCoin.toUpperCase()}: \n${data[coin].usd} USD \n${data[coin].eur} EUR`
+    }
+};
+ 
+
+const usdToCrypto = async (data) => {
+    try {
+        const messageArray = data.split(" ");
+        if (messageArray.length > 2){
+            const valOne = messageArray[1];
+            const symbol = messageArray[2].toLocaleLowerCase();
+            const currency = symbols[symbol];
+            if(isNumeric(valOne) && currency){
+                const data =  await callApi(currency, ['usd'])
+                return `${valOne} USD = ${valOne/data[currency].usd} ${symbol.toUpperCase()}`;
+            }else{
+                return "Comando invalido";
+            }
+        }else{
+            return "Comando invalido";
+        };
+    } catch (error) {
+        return "Comando invalido";
+    };
+};
+
+const prices = async () => {
+    try {
+        const resp = await CoinGeckoClient.simple.price({
+            ids: ["smooth-love-potion", "axie-infinity", "bitcoin", "ethereum"],
+            vs_currencies: ['eur', 'usd'],
+        });
+        const { data } = resp;
+        
+        return `Price of SLP: \n${data["smooth-love-potion"].usd} USD \n${data["smooth-love-potion"].eur} EUR 
+        \nPrice of AXS: \n${data["axie-infinity"].usd} USD \n${data["axie-infinity"].eur} EUR
+        \nPrice of BTC: \n${data.bitcoin.usd} USD \n${data.bitcoin.eur} EUR
+        \nPrice of ETH: \n${data.ethereum.usd} USD \n${data.ethereum.eur} EUR`;
+
+    } catch (error) {
+        return "Comando invalido"
+    }
+}
+
+const breeding = async (data) => {
+    try {
+        const messageArray = data.split(" ");
+        if (messageArray.length > 2){
+            const valOne = messageArray[1];
+            const valTwo = messageArray[2];
+            if(isNumeric(valOne) && valOne >= 0 && valOne < 7 
+                && isNumeric(valTwo) && valTwo >= 0 && valTwo < 7){
+                const resp = await CoinGeckoClient.simple.price({
+                    ids: ["smooth-love-potion", "axie-infinity"],
+                    vs_currencies: ['usd'],
+                });
+                const { data } = resp;
+                return `${(costBreeding[valOne] + costBreeding[valTwo])
+                    * data["smooth-love-potion"].usd
+                    + 4*data["axie-infinity"].usd} USD`;
+            }else{
+                return "Comando invalido";
+            };
+        }else{
+            return "Comando invalido";
+        };
+    } catch (error) {
+        return "Comando invalido";
+    };
+};
+
+module.exports = { getData, breeding, usdToCrypto, prices };
